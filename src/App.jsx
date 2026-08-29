70377aa1441d05d60688e04655c3fff8eceb510b
VinoPraveen <avinopraveen@gmail.com>
2026-08-29 22:35:11 +0530

    added footer section

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import Navbar from './components/Navbar/Navbar.jsx';
 import Home from './pages/Home/Home.jsx';
+import FinalCTA from './components/FinalCTA/FinalCTA.jsx';
+import Footer from './components/Footer/Footer.jsx';
 
 export default function App() {
   return (
     <>
       <Navbar />
       <Home />
+      <FinalCTA />
+      <Footer />
     </>
   );
 }
